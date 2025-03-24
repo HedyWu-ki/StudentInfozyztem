cbe29e76d44fa31f68347c9c691016785c5690da
HedyWu-ki <2224534997@qq.com>
2025-03-24 10:48:45 +0800

    无关

--- src/sb/HelloWorld.java
+++ src/sb/HelloWorld.java
@@ -3,6 +3,5 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
         System.out.println("Hello, the World!");
-        System.out.println("Hello, the 去你的 fucking World!");
     }
 }
